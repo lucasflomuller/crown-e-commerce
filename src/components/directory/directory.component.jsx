6081f6a5af8be22e30fff64601bba6f3a10166e3
lucasflomuller <lucasflorianomuller@gmail.com>
2019-10-15 17:06:04 -0300

    add routing withRouterHOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -9,36 +9,41 @@ class Directory extends React.Component {
       {
         title: "hats",
         imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-        id: 1
+        id: 1,
+        linkUrl: "hats"
       },
       {
         title: "jackets",
         imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-        id: 2
+        id: 2,
+        linkUrl: ""
       },
       {
         title: "sneakers",
         imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-        id: 3
+        id: 3,
+        linkUrl: ""
       },
       {
         title: "womens",
         imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
         size: "large",
-        id: 4
+        id: 4,
+        linkUrl: ""
       },
       {
         title: "mens",
         imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
         size: "large",
-        id: 5
+        id: 5,
+        linkUrl: ""
       }
     ]
   };
 
   render() {
-    const sections = this.state.sections.map(section => (
-      <MenuItem {...section} />
+    const sections = this.state.sections.map(({ id, ...section }) => (
+      <MenuItem key={id} {...section} />
     ));
 
     return <div className="directory-menu">{sections}</div>;
